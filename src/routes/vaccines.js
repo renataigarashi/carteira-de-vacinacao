2edb96bd3651701a43da5004eb6b6faae4406a82
Renata <rsigarashi@hotmail.com>
2021-11-30 00:25:28 -0300

    Correção rota PATCH

--- src/routes/vaccines.js
+++ src/routes/vaccines.js
@@ -5,6 +5,6 @@ const controller = require("../controllers/vaccineController");
 router.post("/", controller.createVaccine);
 router.get("/", controller.getAllVaccine);
 router.get("/:id", controller.getVaccineById);
-router.patch("/:id", controller.updateVaccinated)
+router.patch("/:id/vaccinated", controller.updateVaccinated)
 
 module.exports = router
